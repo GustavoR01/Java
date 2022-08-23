2e215c0c01e49c8ee60aeae9db69cb6c9e5cb640
GustavoR01 <111398007+GustavoR01@users.noreply.github.com>
2022-08-22 21:56:51 -0300

    Add files via upload

---
+++ JAVA 2/src/App.java
@@ -0,0 +1,46 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      int idade, num, teste;
+      double salario, altura;
+      float peso;
+      char digito; 
+      long numero_grande;
+      short numero_curto;
+      byte pequeno;
+      boolean resposta;
+      String nome;
+
+
+      int code = 123;
+      float preco = 12.35F;
+      char letra = 'A';
+
+      int cod1, cod2;
+      cod1 = 54;
+      cod2 = cod1;
+
+      Scanner leia = new Scanner(System.in);
+        
+        System.out.println(x: "Digite seu nome: ");
+        nome = leia.nextLine();
+        System.out.println(x: "Digite seu peso: ");
+        peso = leia.nextFloat()
+        
+        System.out.println(x: "digite sua idade: ");
+        idade = leia.nextInt();
+        System.out.println(x: "Você é uma pessoa? ");
+        resposta = leia.nextBoolean();
+        JOptionPane.showMessageDialog(null, "");
+      
+
+      
+        System.out.println("Hello, World!");
+
+        JOptionPane.showMessageDialog(null, " E ai, tranquilo? \n **");
+       
+    
+    }
+}
